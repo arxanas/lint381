84cbab85dd18ef3ee3c8e4749975a6e1923b3c5a
Waleed Khan <me@waleedkhan.name>
2016-01-13 17:17:40 -0500

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="lint381",
-    version="0.2.1",
+    version="0.3.0",
     author="Waleed Khan",
     author_email="wkhan@umich.edu",
     description="C and C++ linter for EECS 381.",
