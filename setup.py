887e1428bd1b603f5f8a152d138b8135a512e09c
Waleed Khan <me@waleedkhan.name>
2016-01-27 01:04:45 -0500

    Release version 0.5.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="lint381",
-    version="0.5.1",
+    version="0.5.2",
     author="Waleed Khan",
     author_email="wkhan@umich.edu",
     description="C and C++ linter for EECS 381.",
