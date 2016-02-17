23fcb370b3f7c37a20490d95e9c676dd1206af26
Waleed Khan <me@waleedkhan.name>
2016-02-17 17:16:49 -0500

    Release version 1.3.2
    Changes:
    
      * Flag putting user includes after system includes.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="lint381",
-    version="1.3.1",
+    version="1.3.2",
     author="Waleed Khan",
     author_email="wkhan@umich.edu",
     description="C and C++ linter for EECS 381.",
